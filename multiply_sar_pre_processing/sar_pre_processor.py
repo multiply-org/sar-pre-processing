17c30eb5e9158953487a4d4d551aa52eefb25e53
McWhity <weiss.thomas@lmu.de>
2017-11-13 17:53:58 +0100

    add new class AttributeDict, import configuration through .yaml config file is working

--- multiply_sar_pre_processing/sar_pre_processor.py
+++ multiply_sar_pre_processing/sar_pre_processor.py
@@ -3,6 +3,32 @@ Wrapper module to launch preprocessor
 """
 
 import os
+import yaml
+
+import pdb
+
+class AttributeDict(object):
+    """
+    A class to convert a nested Dictionary into an object with key-values
+    accessibly using attribute notation (AttributeDict.attribute) instead of
+    key notation (Dict["key"]). This class recursively sets Dicts to objects,
+    allowing you to recurse down nested dicts (like: AttributeDict.attr.attr)
+    """
+    def __init__(self, **entries):
+        self.add_entries(**entries)
+
+    def add_entries(self, **entries):
+        for key, value in entries.items():
+            if type(value) is dict:
+                self.__dict__[key] = AttributeDict(**value)
+            else:
+                self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        """
+        Provides dict-style access to attributes
+        """
+        return getattr(self, key)
 
 
 class PreProcessor(object):
@@ -10,14 +36,25 @@ class PreProcessor(object):
     def __init__(self, **kwargs):
         self.config = kwargs.get('config', None)
         self._check()
+        self._get_config()
 
-        self.gpt = self.config.gpt
+        # self.gpt = self.config.gpt
+        pdb.set_trace()
 
     def _check(self):
         assert self.config is not None, 'ERROR: configuration needs to be provided'
 
     def pre_process(self):
         assert False, 'Routine should be implemented in child class'
+
+    def _get_config(self):
+        """
+        Load configuration from self.config.
+           writes to self.config.
+        """
+        with open(self.config, 'r') as cfg:
+            self.config = yaml.load(cfg)
+            self.config = AttributeDict(**self.config)
 
 
 class SARPreProcessor(PreProcessor):
@@ -51,10 +88,10 @@ class SARPreProcessor(PreProcessor):
         assert output_folder is not None, 'ERROR: output folder needs to be specified'
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
-        
+
         # Name addition for processed data
         xml_addition = 'subset'
-        
+
         lower_right_y = self.config.region['lr']['lat']
         upper_left_y = self.config.region['ul']['lat']
         upper_left_x = self.config.region['ul']['lon']
@@ -65,9 +102,9 @@ class SARPreProcessor(PreProcessor):
         area = self.get_area(lower_right_y, upper_left_y, upper_left_x, lower_right_x)
 
         # o.k., now the rest of the preprocessor can be added here
-        # to keep things most flexible it would be good to have that in 
+        # to keep things most flexible it would be good to have that in
         # a separate class
-        
+
         return 'some data'
 
     def get_area(self, lat_min, lat_max, lon_min, lon_max):
@@ -76,6 +113,4 @@ class SARPreProcessor(PreProcessor):
         """
         assert lat_min <= lat_max, 'ERROR: invalid lat'
         assert lon_min <= lon_max, 'ERROR: invalid lon'
-        return '%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f' % (lon_min, lat_min, lon_min, lat_max,
-                                                                                lon_max, lat_max, lon_max, lat_min,
-                                                                                lon_min, lat_min)
+        return '%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f' % (lon_min, lat_min, lon_min, lat_max, lon_max, lat_max, lon_max, lat_min, lon_min, lat_min)
