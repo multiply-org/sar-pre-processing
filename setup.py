3cb8e66b2c5f3b97a1ed2523c979f3b4faf33c43
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-02-05 18:07:14 +0100

    fixed package name

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from setuptools import setup
 
-setup(name='multiply-sar-pre-processing',
+setup(name='sar-pre-processing',
       version='0.1',
       description='MULTIPLY SAR Pre-Processing',
       author='MULTIPLY Team',
-      packages=['multiply_sar_pre_processing'],
+      packages=['sar_pre_processing'],
      )
