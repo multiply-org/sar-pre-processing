99a1b523c9836fa05a31b26dcc78913775d6c737
Thomas Weiß <weiss.thomas@lmu.de>
2020-07-31 14:11:37 +0200

    remove extension IPython.sphinxext.ipython_console_highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -45,8 +45,7 @@ sys.path.insert(0, _pysrc)
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.coverage',
               'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
-              'sphinx.ext.napoleon', 'sphinxcontrib.bibtex', 'nbsphinx',
-              'IPython.sphinxext.ipython_console_highlighting']
+              'sphinx.ext.napoleon', 'sphinxcontrib.bibtex', 'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
